faa43d8130a69b09d8f3dcb3e1c87aed610bd6fc
Jeeglobs <dp.hudgins@hotmail.com>
2023-03-15 10:14:18 -0400

    created add_habit view and url and template

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('habits/new', views.add_habit, name='add_habit'),
     path('accounts/', include('registration.backends.simple.urls')),
 ]
